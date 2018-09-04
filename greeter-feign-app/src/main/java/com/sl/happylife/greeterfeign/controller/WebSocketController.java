1f5cef20328b90d3df9d8796b010248d7c81c8bf
suxin <suxin@baozhunniu.cc>
2018-09-04 16:26:14 +0800

    添加Controller

---
+++ greeter-feign-app/src/main/java/com/sl/happylife/greeterfeign/controller/WebSocketController.java
@@ -0,0 +1,46 @@
+package com.sl.happylife.greeterfeign.controller;
+
+import com.sl.happylife.greeterfeign.domain.dto.Greeting;
+import com.sl.happylife.greeterfeign.domain.dto.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class WebSocketController {
+
+    @RequestMapping(path = "/websocket/index", method = RequestMethod.GET)
+    public String toWebSocketPage() {
+
+        return "/websocket/index";
+    }
+
+    @RequestMapping(path = "/websocket/toUser", method = RequestMethod.GET)
+    public String toWebSocketUserPage() {
+
+        return "/websocket/toUser";
+    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+//
+//    @MessageMapping("/hello")
+//    public Greeting hello(HelloMessage message) {
+//
+//        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+//    }
+//
+//    @SendTo("/topic/greetings")
+//    public Greeting toMessage() {
+//
+//        return new Greeting("Hello !");
+//    }
+
+}
